b4ce6e76edc757f704b9fffbfccd4bcc08c4ae5e
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2021-02-11 15:37:01 +0200

    removed hibernateUtil class and lib packages, added spring and spring dependecy in project, added helloController class and method sayHello

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -1,68 +1,8 @@
 package com.dev.cinema;
 
-import com.dev.cinema.exception.AuthenticationException;
-import com.dev.cinema.lib.Injector;
-import com.dev.cinema.model.CinemaHall;
-import com.dev.cinema.model.Movie;
-import com.dev.cinema.model.MovieSession;
-import com.dev.cinema.model.User;
-import com.dev.cinema.security.AuthenticationService;
-import com.dev.cinema.service.CinemaHallService;
-import com.dev.cinema.service.MovieService;
-import com.dev.cinema.service.MovieSessionService;
-import com.dev.cinema.service.OrderService;
-import com.dev.cinema.service.ShoppingCartService;
-import com.dev.cinema.service.UserService;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.List;
-
 public class Main {
-    private static final Injector injector = Injector.getInstance("com.dev.cinema");
-    private static final MovieService movieService = (MovieService) injector
-            .getInstance(MovieService.class);
-    private static final MovieSessionService movieSessionService = (MovieSessionService)
-            injector.getInstance(MovieSessionService.class);
-    private static final CinemaHallService cinemaHallService = (CinemaHallService)
-            injector.getInstance(CinemaHallService.class);
-    private static final UserService userService = (UserService)
-            injector.getInstance(UserService.class);
-    private static final AuthenticationService authenticationService = (AuthenticationService)
-            injector.getInstance(AuthenticationService.class);
-    private static final ShoppingCartService shoppingCartService = (ShoppingCartService)
-            injector.getInstance(ShoppingCartService.class);
-    private static final OrderService orderService = (OrderService)
-            injector.getInstance(OrderService.class);
 
-    public static void main(String[] args) throws AuthenticationException {
-        Movie movie = new Movie();
-        movie.setTitle("Fast and Furious");
-        movie.setDescription("Interesting action with Vin Diesel in main role");
-        movieService.add(movie);
-        movieService.getAll().forEach(System.out::println);
-        CinemaHall cinemaHall = new CinemaHall();
-        cinemaHall.setCapacity(10);
-        cinemaHall.setDescription("Fast");
-        cinemaHallService.add(cinemaHall);
-        cinemaHallService.getAll().forEach(System.out::println);
-        MovieSession movieSession = new MovieSession();
-        movieSession.setMovie(movie);
-        movieSession.setCinemaHall(cinemaHall);
-        movieSession.setShowTime(LocalDateTime.of(2021, Month.MARCH, 23,
-                15, 0, 0));
-        movieSessionService.add(movieSession);
-        List<MovieSession> movieSessions = movieSessionService.findAvailableSessions(movie.getId(),
-                LocalDate.of(2021, Month.MARCH, 23));
-        System.out.println(movieSessions.toString());
-        authenticationService.register("example@gmail.com", "bob123");
-        User user = authenticationService.login("example@gmail.com", "bob123");
-        User user2 = authenticationService.register("123@gmail.com", "123");
-        shoppingCartService.addSession(movieSession, user);
-        shoppingCartService.addSession(movieSession, user2);
-        shoppingCartService.clear(shoppingCartService.getByUser(user2));
-        System.out.println(shoppingCartService.getByUser(user));
-        orderService.completeOrder(shoppingCartService.getByUser(user));
-        System.out.println(orderService.getOrdersHistory(user));
+    public static void main(String[] args) {
+
     }
 }
--- src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
@@ -2,23 +2,28 @@ package com.dev.cinema.dao.impl;
 
 import com.dev.cinema.dao.MovieSessionDao;
 import com.dev.cinema.exception.DataProcessingException;
-import com.dev.cinema.lib.Dao;
 import com.dev.cinema.model.MovieSession;
-import com.dev.cinema.util.HibernateUtil;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
 
-@Dao
+@Repository
 public class MovieSessionDaoImpl implements MovieSessionDao {
+    private final SessionFactory sessionFactory;
+
+    public MovieSessionDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             LocalDateTime startOfDay = date.atStartOfDay();
             LocalDateTime endOfDay = date.atTime(LocalTime.MAX);
             Query<MovieSession> movieQuery = session.createQuery("from MovieSession ms "
@@ -40,7 +45,7 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
         Session session = null;
         Transaction transaction = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = sessionFactory.openSession();
             transaction = session.beginTransaction();
             session.save(movieSession);
             transaction.commit();
