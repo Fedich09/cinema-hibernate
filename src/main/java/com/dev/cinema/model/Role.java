56deb1e27be65c866b130119956b2990e2bb36d6
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2021-02-21 17:45:36 +0200

    added /@JoinTable(name = "role_name")

--- src/main/java/com/dev/cinema/model/Role.java
+++ src/main/java/com/dev/cinema/model/Role.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
 import javax.persistence.Table;
 
 @Entity
@@ -13,6 +14,7 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JoinTable(name = "role_name")
     private String roleName;
 
     public String getRoleName() {
